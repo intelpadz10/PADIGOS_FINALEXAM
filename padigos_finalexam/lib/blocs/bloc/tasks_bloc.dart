b0d37d5edcee9e87fb692a96c2e3b5cdb2e164a9
Intel Padz <padigos1000@gmail.com>
2022-08-03 14:17:30 +0800

    added Add Task Feature

---
+++ padigos_finalexam/lib/blocs/bloc/tasks_bloc.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:padigos_finalexam/blocs/bloc/tasks_event.dart';
+import 'package:padigos_finalexam/blocs/bloc/tasks_state.dart';
+import 'package:padigos_finalexam/models/task.dart';
+
+class TasksBloc extends HydratedBloc<TasksEvent, TasksState> {
+  TasksBloc() : super(const TasksState()) {
+    on<AddTask>(_onAddTask);
+    on<UpdateTask>(_onUpdateTask);
+    on<DeleteTask>(_onDeleteTask);
+  }
+
+  void _onAddTask(AddTask event, Emitter<TasksState> emit) {
+    final state = this.state;
+    emit(TasksState(
+      allTasks: List.from(state.allTasks)..add(event.task),
+    ));
+  }
+
+  void _onUpdateTask(UpdateTask event, Emitter<TasksState> emit) {
+    final state = this.state;
+    final task = event.task;
+    final int index = state.allTasks.indexOf(task);
+    List<Task> allTasks = List.from(state.allTasks)..remove(task);
+
+    task.isDone == false
+        ? allTasks.insert(index, task.copyWith(isDone: true))
+        : allTasks.insert(index, task.copyWith(isDone: false));
+    emit(TasksState(
+      allTasks: allTasks,
+    ));
+  }
+
+  void _onDeleteTask(DeleteTask event, Emitter<TasksState> emit) {
+    final state = this.state;
+    emit(TasksState(
+      allTasks: List.from(state.allTasks)..remove(event.task),
+    ));
+  }
+  
+  @override
+  TasksState? fromJson(Map<String, dynamic> json) {
+    return TasksState.fromMap(json);
+  }
+  
+  @override
+  Map<String, dynamic>? toJson(TasksState state) {
+    return state.toMap();
+  }
+}
--- padigos_finalexam/lib/main.dart
+++ padigos_finalexam/lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:padigos_finalexam/blocs/bloc/tasks_event.dart';
+import 'package:padigos_finalexam/models/task.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'app_router.dart';
 import 'app_themes.dart';
+import 'blocs/bloc_exports.dart';
 import 'screens/tabs_screen.dart';
 
-void main() {
-  runApp(
-    MyApp(appRouter: AppRouter()),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory());
+  HydratedBlocOverrides.runZoned(
+    () => runApp(MyApp(appRouter: AppRouter())),
+    storage: storage,
   );
 }
 
@@ -17,12 +27,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'BloC Tasks App',
-      theme: AppThemes.appThemeData[AppTheme.lightMode],
-      home: const TabsScreen(),
-      onGenerateRoute: appRouter.onGenerateRoute,
+    return BlocProvider(
+      create: (context) => TasksBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'BloC Tasks App',
+        theme: AppThemes.appThemeData[AppTheme.lightMode],
+        home: const TabsScreen(),
+        onGenerateRoute: appRouter.onGenerateRoute,
+      ),
     );
   }
 }
